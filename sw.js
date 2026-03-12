3e77a5b797570ad9cb1a9992be9286b5fd6307ac
kaua-souza7 <kssouza344@gmail.com>
2026-03-12 19:19:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "financeiro-v1";
+
+const urlsToCache = [
+  "./",
+  "./controle-financeiro.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
